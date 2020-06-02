012e5350bc36b5151cc922629378f964c624c323
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-02 16:06:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist=(artist)
-    self.genre=(genre)
+    self.artist=(artist) if artist
+    self.genre=(genre) if genre
   end
 
   def self.all
@@ -19,16 +19,14 @@ class Song
   end
 
   def artist=(artist)
-    if artist && self.artist == nil
+    if self.artist == nil
       @artist = artist
       artist.add_song(self)
     end
   end
 
   def genre=(genre)
-    if genre
-      genre.songs << self unless genre.songs.include?(self)
-    end
+    genre.songs << self unless genre.songs.include?(self)
     @genre = genre
   end
 
